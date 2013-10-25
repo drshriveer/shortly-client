b04a6bd1b0e0e85a48f0dec234b520c3302a3fc3
Gavin Shriver <gavin.shriver@gmail.com>
2013-10-25 16:55:57 -0700

    refactors to use routes and templates

--- public/client/app.js
+++ public/client/app.js
@@ -1,5 +1,24 @@
 var app = angular.module('Shortly',[])
-  .controller("ShortlyController", function($scope, $http){
+  .config(function($routeProvider){
+    $routeProvider
+      .when('/',{
+        controller: "LinksCtrl",
+        // template: "<h1> I'm a template! </h1>"
+        templateUrl: "templates/links.html"
+      })
+      .when('/create',{
+        controller: "CreateCtrl",
+        templateUrl: "templates/create.html"
+      })
+      .otherwise({
+        redirectTo: '/'
+      })
+  })
+  //controllers: 
+  .controller('createCtrl',function($scope, $http){
+    //---
+  })
+  .controller("LinksCtrl", function($scope, $http){
     $http.get('/links')
       .success(function(data, status, headers, config){
         $scope.links = data;

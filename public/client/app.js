74b6ab9724c9944ba231b6be91bd57b16965bf28
Gavin Shriver <gavin.shriver@gmail.com>
2013-10-26 11:07:34 -0700

    appends link after shortening url

--- public/client/app.js
+++ public/client/app.js
@@ -18,12 +18,15 @@ var app = angular.module('Shortly',[])
   .controller('CreateCtrl',function($scope, $http){
 
     $scope.post = function(){
+      // TODO: start spinner
       $http.post('/links', {url: $scope.url})
         .success(function(data, status, headers, config){
-          // TODO: append to link view
-          console.log('Post was successful');
+          // TODO: stop spinner!
+          $scope.links = $scope.links || [];
+          $scope.links.push(data);
         })
         .error(function(data, status){
+          // TODO: stop spinner!
           console.log('Post did not succeed.');
         });
     }

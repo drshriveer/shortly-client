1f4f99bae99e498b8a5fd4769009189263d2bece
Gavin Shriver <gavin.shriver@gmail.com>
2013-10-26 18:22:32 -0700

    refactors more and adds url validation on client side using angular's built in validation

--- public/client/app.js
+++ public/client/app.js
@@ -21,44 +21,53 @@ var app = angular.module('Shortly',[])
 
     //LINK SERVICE:
 app.factory('linkService', function($http, $q){
-    var service = {
-      getLinks: function(){
-        var deferred = $q.defer();
-        $http({
-          method: 'GET',
-          url: '/links'
-        }).success(function(data){
-          deferred.resolve(data);
-        }).error(function(error){
-          deferred.reject(error)
-        })
-        return deferred.promise;
-      }
-    };
-    return service; 
-  })
-
-    //LINK CONTROLLERS:
-app.controller("LinksCtrl", function($scope, linkService , $http){
-    linkService.getLinks().then(function(data){
-      $scope.links = data;
-    });
+  var service = {
+    getLinks: function(){
+      var deferred = $q.defer();
+      $http({
+        method: 'GET',
+        url: '/links'
+      }).success(function(data){
+        deferred.resolve(data);
+      }).error(function(error){
+        deferred.reject(error)
+      })
+      return deferred.promise;
+    },
+    createLink: function(url){
+      var deferred = $q.defer();
+      $http({
+        method: "POST",
+        url: '/links',
+        data: {url: url}
+      }).success(function(data){
+        deferred.resolve(data);
+      }).error(function(error){
+        deferred.reject(error);
+      });
+      return deferred.promise;
+    }
+  };
+  return service; 
 });
 
     //LINK CONTROLLERS:
-app.controller('CreateCtrl',function($scope, $http){
-
-    $scope.post = function(){
-      // TODO: start spinner
-      $http.post('/links', {url: $scope.url})
-        .success(function(data, status, headers, config){
-          // TODO: stop spinner!
-          $scope.links = $scope.links || [];
-          $scope.links.push(data);
-        })
-        .error(function(data, status){
-          // TODO: stop spinner!
-          console.log('Post did not succeed.');
-        });
-    }
+app.controller("LinksCtrl", function($scope, $http, linkService){
+  linkService.getLinks().then(function(data){
+    $scope.links = data;
   });
+});
+
+    //LINK CONTROLLERS:
+app.controller('CreateCtrl',function($scope, $http, linkService){
+  //should check if input has valid url
+  $scope.post = function(){
+    if($scope.urlForm.urlBox.$valid){
+      linkService.createLink($scope.url).then(function(data){
+        $scope.links = $scope.links || [];
+        $scope.links.push(data);
+        $scope.url = "";  //chear the data
+      });
+    }
+  }
+});
